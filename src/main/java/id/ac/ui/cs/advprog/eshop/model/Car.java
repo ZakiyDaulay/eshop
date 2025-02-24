02ed688cb368083db54829212e5185929b400bfc
ZakiyDaulay <zakiydaulay@gmail.com>
2025-02-24 15:35:03 +0700

    added car.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Car {
+    private String CarId;
+    private String CarName;
+    private String CarColor;
+    private int carQuantity;
+}
