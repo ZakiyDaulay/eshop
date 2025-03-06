1a3b3148d178a73708e2a8448f3b39b4d8319099
ZakiyDaulay <zakiydaulay@gmail.com>
2025-03-06 22:01:26 +0700

    [REFACTOR] apply paymentstatus enum to payment model and tests

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.model;
-
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +10,10 @@ public class Payment {
     private final String id;
     private final String method;
     @Setter
-    private String status;
+    private PaymentStatus status;
     private final Map<String, String> paymentData;
 
-    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+    public Payment(String id, String method, PaymentStatus status, Map<String, String> paymentData) {
         this.id = id;
         this.method = method;
         this.status = status;
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentTest {
@@ -10,36 +11,36 @@ public class PaymentTest {
     void testCreatePaymentWithValidData() {
         String id = "1234";
         String method = "Voucher";
-        String status = "SUCCESS";
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
 
-        Payment payment = new Payment(id, method, status, paymentData);
+        Payment payment = new Payment(id, method, PaymentStatus.SUCCESS, paymentData);
 
         assertEquals(id, payment.getId());
         assertEquals(method, payment.getMethod());
-        assertEquals(status, payment.getStatus());
+        assertEquals(PaymentStatus.SUCCESS, payment.getStatus());
         assertEquals(paymentData, payment.getPaymentData());
     }
+
     @Test
     void testPaymentStatusUpdate() {
-        Payment payment = new Payment("1234", "Voucher", "PENDING", new HashMap<>());
-        payment.setStatus("SUCCESS");
-        assertEquals("SUCCESS", payment.getStatus());
+        Payment payment = new Payment("1234", "Voucher", PaymentStatus.PENDING, new HashMap<>());
+        payment.setStatus(PaymentStatus.SUCCESS);
+        assertEquals(PaymentStatus.SUCCESS, payment.getStatus());
     }
+
     @Test
     void testCreatePaymentWithInvalidData() {
         String id = "5678";
         String method = "Voucher";
-        String status = "REJECTED";
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "INVALIDCODE");
 
-        Payment payment = new Payment(id, method, status, paymentData);
+        Payment payment = new Payment(id, method, PaymentStatus.REJECTED, paymentData);
 
         assertEquals(id, payment.getId());
         assertEquals(method, payment.getMethod());
-        assertEquals(status, payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED, payment.getStatus());
         assertEquals(paymentData, payment.getPaymentData());
     }
 }
