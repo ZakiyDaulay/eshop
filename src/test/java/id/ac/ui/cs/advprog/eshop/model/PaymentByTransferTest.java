cad202484fcd327f18f8ca76e21fb8a80b906d83
ZakiyDaulay <zakiydaulay@gmail.com>
2025-03-07 17:34:18 +0700

    added model package in paymentbytransfer

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentByTransferTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentByTransferTest.java
@@ -1,3 +1,5 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.PaymentByTransfer;
 import org.junit.jupiter.api.Test;
